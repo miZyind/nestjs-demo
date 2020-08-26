aaa49bb6ff2efa06ff7c9e71da5ad3b44d2d351a
miZyind <mizyind@gmail.com>
2020-08-26 14:11:29 +0700

    chore: uncomment sql scripts

--- docker/provision/mysql/init/02-users.sql
+++ docker/provision/mysql/init/02-users.sql
@@ -2,6 +2,6 @@ CREATE USER 'nestjs_demo'@'%' IDENTIFIED WITH mysql_native_password BY '123456';
 
 GRANT ALL PRIVILEGES ON nestjs_demo.* TO 'nestjs_demo'@'%';
 
--- GRANT ALL PRIVILEGES ON nestjs_demo_e2e_test.* TO 'nestjs_demo'@'%';
+GRANT ALL PRIVILEGES ON nestjs_demo_e2e_test.* TO 'nestjs_demo'@'%';
 
 FLUSH PRIVILEGES;
